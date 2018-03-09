809be5c6a49206d4dfe51e0b675005c216e8e214
liming26 <liming26@baidu.com>
2018-03-09 16:35:21 +0800

    使用vue-cli搭建测试环境

---
+++ vue_learning/basic/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <HookLearning></HookLearning>
+  </div>
+</template>
+
+<script>
+  import HelloWorld from './components/HelloWorld';
+  import HookLearning from './components/HookLearning';
+
+  export default {
+    name: 'App',
+    components: {
+      HelloWorld,
+      HookLearning
+    }
+  }
+</script>
+
+<style>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+</style>
---
+++ vue_learning/basic/src/components/HookLearning.vue
@@ -0,0 +1,52 @@
+<template>
+  <h1>只是为了测试Vue的生命周期函数</h1>
+</template>
+
+<script>
+  export default {
+    name: "HookLearning",
+    data: function () {
+      return {};
+    },
+    beforeCreate() {
+      console.log(`${new Date().getTime()} in beforeCreate status`);
+    },
+    created() {
+      console.log(`${new Date().getTime()} in created status`);
+    },
+    beforeMount() {
+      console.log(`${new Date().getTime()} in beforeMount status`);
+    },
+    mounted() {
+      console.log(`${new Date().getTime()} in mounted status`);
+    },
+    beforeUpdate() {
+      console.log(`${new Date().getTime()} in beforeUpdate status`);
+    },
+    updated() {
+      console.log(`${new Date().getTime()} in updated status`);
+    },
+    activated() {
+      console.log(`${new Date().getTime()} in activated status`);
+    },
+    deactivated() {
+      console.log(`${new Date().getTime()} in deactivated status`);
+    },
+
+    beforeDestroy() {
+      console.log(`${new Date().getTime()} in beforeDestroy status`);
+    },
+
+    destroyed() {
+      console.log(`${new Date().getTime()} in destroyed status`);
+    },
+    errorCaptured() {
+      console.log(`${new Date().getTime()} in errorCaptured status`);
+    },
+
+  }
+</script>
+
+<style scoped>
+
+</style>
